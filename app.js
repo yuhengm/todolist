ce0c6523d360e29a0aa2f4ba6d7fc5416bc22bbc
Yuheng <miaoyh1997k@gmail.com>
2021-12-28 19:14:20 -0800

    change to local host to see if heroku works

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(
-  "mongodb+srv://admin-yhm:970823@cluster0.mzbwj.mongodb.net/todolistDB",
-  {
-    useNewUrlParser: true,
-  }
-);
+// mongoose.connect(
+//   "mongodb+srv://admin-yhm:970823@cluster0.mzbwj.mongodb.net/todolistDB",
+//   {
+//     useNewUrlParser: true,
+//   }
+// );
 
 // local connection
-// mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {
-//   useNewUrlParser: true,
-// });
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {
+  useNewUrlParser: true,
+});
 
 // Main list items
 const itemSchema = { name: String };
